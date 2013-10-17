84984a0ae771d0950ebbaa43f5e613aa6931dd99
arniegeddon <geddontronic@gmail.com>
2013-10-17 01:09:53 +0000

    Expanded Char creation class

--- shad.py
+++ shad.py
@@ -1,5 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/python2
+
+class character:
+	def __init__(self, name):
+		self.name = name
+
+	def setname(self):
+		self.name = name
+
+	def printname(self):
+		print(self.name)
+
+	def setrace(self, race):
+		self.race = race
+
+	def printrace(self):
+		return self.race
+#		print(self.race)
 
 
-class characterMove():
+class characterMove:
 	pass
+
+mychar = character(raw_input('Enter a name for your character: '))
+mychar.setrace(raw_input('Entr your race: '))
+
+print 'Welcome ' 
+
